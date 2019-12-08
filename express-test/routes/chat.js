519803389dc9a50e7db73336cb851eb1bab06302
soda-private <mails6tt@gmail.com>
2019-12-08 16:32:23 +0000

    chattool

--- express-test/routes/chat.js
+++ express-test/routes/chat.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('chat', { title: 'Chat-Application' });
 });
 
+var io = require('socket.io');
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+    });
+});
+
 module.exports = router;
