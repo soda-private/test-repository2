d33549fac257356cb5357998d2378b96ce95ac82
soda-private <mails6tt@gmail.com>
2019-12-15 10:13:55 +0000

    socketio test

--- express-test/routes/index.js
+++ express-test/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var server = require('http')(express());
+var io = require('socket.io')(server);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
 });
 
 module.exports = router;
